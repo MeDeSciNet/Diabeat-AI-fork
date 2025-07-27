29c5214e4515b044e143b1552d03a070438dbf8d
兩河流域 <teariver0220@gmail.com>
2025-07-27 18:35:06 +0800

    login page: add debug switch and failure dialog

--- glucose-mobi/flutter/diabeat/lib/routes/guest/login.dart
+++ glucose-mobi/flutter/diabeat/lib/routes/guest/login.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'package:diabeat/routes/connection/request.dart';
 import 'package:diabeat/util.dart';
 import 'package:dio/dio.dart';
@@ -20,7 +19,14 @@ class _LoginPageState extends State<LoginPage> {
   bool _obscure = true;
   bool _rememberMe = true;
   bool _submitted = false;
-  bool _onJob = false;
+  bool _waiting = false;
+  bool _debug = true; // debug
+
+  @override
+  void initState() {
+    super.initState();
+    _toggleDebugSwitch(_debug);
+  }
 
   @override
   void dispose() {
@@ -55,7 +61,7 @@ class _LoginPageState extends State<LoginPage> {
 
     if (_emailErr != null || _passwordErr != null) return;
 
-    setState(() => _onJob = true);
+    setState(() => _waiting = true);
 
     try {
       final res = await Request.logIn(
@@ -72,97 +78,147 @@ class _LoginPageState extends State<LoginPage> {
       Navigator.pop(context);
       await Navigator.pushReplacementNamed(context, '/home');
     } on DioException catch (e) {
-      // add ui response
-      if (e.response != null) {
-        log('錯誤狀態碼: ${e.response!.statusCode}');
-        log('錯誤訊息: ${e.response!.data}');
+      if (mounted) {
+        await _LogInFailedDialog.show(context, e.response!.data);
       }
     } finally {
-      setState(() => _onJob = false);
+      setState(() => _waiting = false);
+    }
+  }
+
+  // debug
+  void _toggleDebugSwitch(bool value) {
+    if (value) {
+      setState(() => _debug = true);
+      _emailCtrl.text = 'ray@gmail.com';
+      _passwordCtrl.text = '12345678';
+    } else {
+      setState(() => _debug = false);
+      _emailCtrl.clear();
+      _passwordCtrl.clear();
     }
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      leading: IconButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: const Icon(Icons.arrow_back_ios_new_rounded),
-      ),
-    ),
-    body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Spacer(flex: 1),
-            const Text(
-              '歡迎回來 !',
-              style: TextStyle(fontSize: 35),
-              textAlign: TextAlign.center,
-            ),
-            const Spacer(flex: 1),
-            TextField(
-              controller: _emailCtrl,
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-              decoration: InputDecoration(
-                labelText: '電子信箱',
-                errorText: _emailErr,
-                border: const OutlineInputBorder(),
-              ),
-              onChanged: (value) {
-                if (_submitted) {
-                  setState(_validateEmail);
-                }
-              },
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _passwordCtrl,
-              keyboardType: TextInputType.visiblePassword,
-              textInputAction: TextInputAction.done,
-              decoration: InputDecoration(
-                labelText: '密碼',
-                errorText: _passwordErr,
-                border: const OutlineInputBorder(),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() => _obscure ^= true);
-                  },
-                  icon: _obscure
-                      ? const Icon(Icons.visibility)
-                      : const Icon(Icons.visibility_off),
-                ),
-              ),
-              obscureText: _obscure,
-              onChanged: (value) {
-                if (_submitted) {
-                  setState(_validatePassword);
-                }
-              },
-            ),
-            const SizedBox(height: 20),
-            CheckboxListTile(
-              value: _rememberMe,
-              onChanged: (value) {
-                setState(() => _rememberMe = value!);
-              },
-              title: const Text('記住我'),
-              controlAffinity: ListTileControlAffinity.leading,
-            ),
-            const Spacer(flex: 2),
-            FilledButton(
-              onPressed: _onJob ? null : _tryLogIn,
-              style: BtnStyleExt.mainFilled,
-              child: const Text('登入'),
-            ),
-          ],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
         ),
       ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Spacer(flex: 1),
+              const Text(
+                '歡迎回來 !',
+                style: TextStyle(fontSize: 35),
+                textAlign: TextAlign.center,
+              ),
+              const Spacer(flex: 1),
+              TextField(
+                controller: _emailCtrl,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: '電子信箱',
+                  errorText: _emailErr,
+                  border: const OutlineInputBorder(),
+                ),
+                onChanged: (value) {
+                  if (_submitted) {
+                    setState(_validateEmail);
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _passwordCtrl,
+                keyboardType: TextInputType.visiblePassword,
+                textInputAction: TextInputAction.done,
+                decoration: InputDecoration(
+                  labelText: '密碼',
+                  errorText: _passwordErr,
+                  border: const OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() => _obscure ^= true);
+                    },
+                    icon: _obscure
+                        ? const Icon(Icons.visibility)
+                        : const Icon(Icons.visibility_off),
+                  ),
+                ),
+                obscureText: _obscure,
+                onChanged: (value) {
+                  if (_submitted) {
+                    setState(_validatePassword);
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              CheckboxListTile(
+                value: _rememberMe,
+                onChanged: (value) {
+                  setState(() => _rememberMe = value!);
+                },
+                title: const Text('記住我'),
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+              const Spacer(flex: 2),
+              SwitchListTile(
+                value: _debug,
+                onChanged: _toggleDebugSwitch,
+                title: const Text('Debug'),
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+              FilledButton(
+                onPressed: _waiting ? null : _tryLogIn,
+                style: BtnStyleExt.mainFilled,
+                child: const Text('登入'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LogInFailedDialog extends StatelessWidget {
+  const _LogInFailedDialog._(this._data);
+  final dynamic _data;
+
+  static Future<void> show(BuildContext context, dynamic data) async {
+    await showDialog(
+      context: context,
+      builder: (context) => _LogInFailedDialog._(data),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => AlertDialog(
+    title: Center(child: Text('登入失敗')),
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(_data['non_field_errors'][0], textAlign: TextAlign.center),
+        const SizedBox(height: 20),
+        FilledButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('OK'),
+        ),
+      ],
     ),
   );
 }
